c3b7c3b724e2de104c4e2564e8719f467cd6bf1a
abdullah-mumin <amumin628@gmail.com>
2021-10-04 12:29:09 +0600

    nab added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="primary" variant="light">
+                <Container className="m-2">
+                    <Navbar.Brand>
+                        <NavLink to="/home" className="text-white text-decoration-none">
+                            King's United
+                        </NavLink>
+                    </Navbar.Brand>
+                    <Nav className="me-auto gap-3" activeKey="/home">
+                        <NavLink className="text-white text-decoration-none" to="/home">Home</NavLink>
+                        <NavLink className="text-white text-decoration-none" to="/services">Services</NavLink>
+                        <NavLink className="text-white text-decoration-none" to="/levels">Levels</NavLink>
+                        <NavLink className="text-white text-decoration-none" to="/about">About us</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Levels/Levels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Levels = () => {
+    return (
+        <div>
+            <h2>Levels</h2>
+        </div>
+    );
+};
+
+export default Levels;
